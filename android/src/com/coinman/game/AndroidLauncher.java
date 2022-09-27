a6d503f7697764292249e6ce31dddfb42b8dcb57
Rachit <bhojanirachit@gmail.com>
2022-09-27 11:10:29 +0530

    Initial commit

---
+++ android/src/com/coinman/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.coinman.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.coinman.game.Coinman;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Coinman(), config);
+	}
+}
